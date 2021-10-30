4a52b500bb9622114450a6d4c695ba324cce6f9b
Yarasmy Jure <yrjure@uc.cl>
2021-10-30 14:52:55 -0300

    Finalizacion carga funciones

--- Funciones niveles superiores.R
+++ Funciones niveles superiores.R
@@ -1,5 +1,5 @@
 
-#Calculo del micro indice de producto(MIPRO)
+#Calculo del micro indice de producto(MIPRO)#----
 
 MIPRO_t.p = function(datos, t, p ){
   MIPRO = NULL
@@ -14,7 +14,7 @@ MIPRO_t.p = function(datos, t, p ){
 
 MIPRO_t.p(data,1,1)
 
-#Calculo del micro indice de subclase(MISCL)
+#Calculo del micro indice de subclase(MISCL)#----
 MISCL_t.s = function(datos, t, s, p){
   sum(MIPRO_t.p(datos, t, p)*Wpro_p(p)/Wscl_s(s))
 }
@@ -23,18 +23,52 @@ MISCL_t.s = function(datos, t, s, p){
 ######SE NECESITA GUARDAR PONDERACION DE la sub clase COMO Wscl_s #########
 
 
-#Indice de la clase
+#Indice de la subclase#----
 
 ISCL_t.s = function(datos, t ,s){
   MISCL_t.s(datos, t, s)/MISCL_t.s(datos, t-1, s)*100
 }
 
-#Calculo del micro indice de clase (MICLA)
+#Calculo del micro indice de clase (MICLA) #----
 
-MICLA_t.c = function(t,c){
+MICLA_t.c = function(datos, t, c){
   
   sum(MISCL_t.s(datos, t, s)*Wscls(s)/Wcla_c(c))
 }
 
+# Indice de clase  #----
+
+ICLA_t.c = function(datos, t, c){
+  MICLA_t.c(datos, t, c)/MICLA_t.c(datos, t-1, c)*100
+}
+
+# Micro indice de grupo (MIGRU)#----
+
+MIGRU_t.g = function(datos, t, g){
+  sum(MICLA_t.c(datos, t, c)*Wcla_c(c)/Wgru_g(g))
+}
+  
+#Indice de grupo (IGRU)#----
+
+IGRU_t.g= function(datos, t, g){
+  MIGRU_t.g(datos, t, g) /MIGRU_t.g(datos, t-1, g)*100
+}
 
 
+#Calculo del micro indice de division (MIDIV)
+
+MIDIV_t.d = function(datos, t, d){
+  sum(MIGRU_t.g(datos, t, g)*Wgru_g(g)/Wdiv_d(d))
+}
+
+## Indice al precio al consumidor (IPC)
+
+
+IPC_t = function(datos,t){
+  N = length(Lista_productos)
+  ipc= 0
+  for( i in 1:N){
+    ipc =+ MIPRO_t.p(datos, t, i)*Wprop_p(i)
+  }
+  return(ipc)
+}
