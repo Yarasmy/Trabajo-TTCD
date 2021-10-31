35b39578cf8813761f22d0b17036dcff2d665f71
Yarasmy Jure <yrjure@uc.cl>
2021-10-31 13:23:43 -0300

    Cambio de base de datos

--- Codigos/Ponderacion.R
+++ Codigos/Ponderacion.R
@@ -1,23 +1,23 @@
 
 library(readxl)
-ponderaciones = read_excel("ipc-xls.xlsx", 
+ponderaciones = read_excel("ipc-base-2018-serie-referencial-xls.xlsx", 
                              sheet = "IPC Base 2018=100", skip = 3)
 
 
 index2 = c()
 
 for (i in 1:length(Lista_productos)) {
-  
   lol = which(Lista_productos[i] == ponderaciones$Glosa)
   index2= c(index2,lol)
-  
 }
 
 ponde = ponderaciones[index2,]
+unique(ponde$Glosa)
 
-ponde1= na.omit(ponde)
+## Ponderadores
 
-
+ponde = ponde[,1:10]
+ponde1 = na.omit(ponde)
 
 pond= c()
 productos= c()
@@ -25,7 +25,7 @@ N=dim(ponde1)[1]
 #aaa= c()
 #mmm= c()
 for(i in 1:N){
-  if(ponde1$Año[i] == 2020){
+  if(ponde1$Año[i] == 2018){
     #aaa= c(aaa,ponde1$Año[i])
     if(ponde1$Mes[i] == 1){
       #mmm= c(mmm, ponde1$Mes[i])
@@ -34,32 +34,27 @@ for(i in 1:N){
     }
   }
 }
-#261
+
+
 sum(pond)
-dim(ponde1)
 
-productos
-
-#5481/261#21
-## comentarios son para comprobar el funcionamiento de la funcion
-
+##Chequear que se uso toda la lista de productos
 diferentes = setdiff(Lista_productos, productos)
 diferentes
+#Perfecto :)
 
-## 8 elementos sin ponderacion
-
+##Reescalamos
 pond= pond/sum(pond)*100
-Lista_productos=productos
 
 
-data_sub= data[,1:8]
+data_sub= data[,1:8] 
 data_sub= cbind(data_sub,PONDERACION= 0)
 head(data_sub)
+
 
 N=dim(data_sub)[1]
 N
 
-sum(table(data_sub$Glosa_Producto))
 ##Los datos se repiten muchas veces 
 rep = as.numeric(table(data_sub$Glosa_Producto))
 nom_rep =names(table(data_sub$Glosa_Producto))
@@ -79,6 +74,10 @@ for(i in 1:N){
 head(data_sub)
 ##Recordar que se reajusto
 
+###############################################################################
+
+##Por analizar
+
 Wscl= c(0,0,0,0)
 N=dim(data_sub)[1]
 for(i in 1:N){
@@ -89,10 +88,9 @@ for(i in 1:N){
     }
   }
 }
-
 Wscl
 sum(Wscl)
 
-
+###############################################################################
 
 
