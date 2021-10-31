6041fc2b5ae7cea6085e3502180c68f8a49253e3
Yarasmy Jure <yrjure@uc.cl>
2021-10-31 08:05:19 -0300

    Intento cambio codigo

--- Codigos/Ponderacion.R
+++ Codigos/Ponderacion.R
@@ -45,6 +45,7 @@ for(i in 1:N){
 #261
 sum(pond)
 dim(ponde1)
+
 productos
 
 #5481/261#21
@@ -59,8 +60,49 @@ pond= pond/sum(pond)*100
 Lista_productos=productos
 
 
-
-
-
 data_sub= data[,1:8]
-data_sub= rbind(data_sub, 0)
+data_sub= cbind(data_sub,PONDERACION= 0)
+head(data_sub)
+
+N=dim(data_sub)[1]
+N
+
+for(i in 1:N){
+  for(j in 1:length(productos)){
+    if(productos[j] == data_sub$Glosa_Producto[i]){
+      data_sub$PONDERACION[i] = pond[j]
+    } 
+  }
+}
+head(data_sub)
+##Recordar que se reajusto
+
+sum(table(data_sub$Glosa_Producto))
+##Los datos se repiten muchas veces 
+rep = as.numeric(table(data_sub$Glosa_Producto))
+nom_rep =names(table(data_sub$Glosa_Producto))
+
+
+Wscl= c(0,0,0,0)
+N=dim(data_sub)[1]
+for(i in 1:N){
+  ##Son 4 subclases
+  for(j in 1:4){
+    if(data_sub$SUBCLASE[i] == j){
+      for(k in 1:269){
+        if(data_sub$Glosa_Producto[i] == nom_rep[k]){
+          div=rep[k]
+        }
+      }
+      Wscl[j]= Wscl[j]+data_sub$PONDERACION[i]/div
+      #Para reajustar
+    }
+  }
+}
+
+Wscl
+sum(Wscl)
+
+
+
+
